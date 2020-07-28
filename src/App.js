8872bf66609db55201ee400f2e30d717f8148f45
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-07-28 15:52:40 +0200

    Created basic structure that uses useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+const items = [
+  { title: "Who are you?", content: "Batman" },
+  { title: "Who is your butler?", content: "Alfred" },
+  { title: "Who is your nemesis?", content: "Joker" },
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items}></Accordion>
     </div>
   );
-}
-
-export default App;
+};
