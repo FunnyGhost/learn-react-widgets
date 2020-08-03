1bf968096ad248b7eb7d4b104b36dbb405bd761a
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-08-03 16:38:54 +0200

    Use the cleanup function from useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,7 +20,11 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) search();
+    const timeoutId = setTimeout(() => {
+      if (term) search();
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
   }, [term]);
 
   const renderedResults = results.map((result) => {
